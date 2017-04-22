b90a2ae65667bd1130d2641d75a976d18b472fe6
Ernesto Soto <e.soto162@uniandes.edu.co>
2017-04-22 09:06:39 -0500

    Subir code refactoring

---
+++ Code_Refactoring/refactoring.php
@@ -0,0 +1,45 @@
+<?php
+
+public function post_confirm() {
+	$id = Input::get('service_id'); 
+	$servicio = Service::find($id);
+	$driverId = Input::get('driver_id');
+	if ($servicio != NULL) {
+		if ($servicio->status_id== '6'){
+			return Response::json(array('error' => '2'));
+		}
+		if ($servicio->driver_id == NULL && $servicio->status_id== '1'){
+			$servicio = Service::update($id, array(
+				'driver_id' => $driverId, 
+				'status_id' => '2'
+			)); 
+			Driver::update($driverId, array(
+				'available' => '0' 
+			)); 
+			$driverTmp = Driver::find($driverId);
+			Service::update($id, array(
+				'car_id' => $driverTmp->car_id
+			));
+			//Notificar a usuario!!
+			$pushMessage = 'Tu servicio ha sido confirmado!'; 
+			$push = Push::make();
+			if ($servicio->user->uuid == ''){
+				return Response::json(array('error' => '0'));
+			}
+			if ($servicio->user->type == '1') { //iPhone
+				$result = $push->ios($servicio->user->uuid,$pushMessage, 1, 'honk.wav', 'Open', array ('serviceId' => $servicio->id)); 
+			} else {
+				$result = $push->android2($servicio->user->uuid, $pushMessage, 1, 'default', 'Open', array ('serviceId' => $servicio->id)); 
+			}
+			return Response::json(array('error' => '0')); 
+		} 
+		else {
+			return Response::json(array('error' => '1'));
+		} 
+	} 
+	else {
+		return Response::json(array('error' => '3')); 
+	}
+}
+
+?>
